6bf72b796ceed370d72d6ec2c35abb0fd98a29b1
jennaly <106183040+jennaly@users.noreply.github.com>
2022-10-21 13:52:39 -0700

    move positioning of social icons

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -4,7 +4,7 @@ import Twitter from "../assets/twitter-icon.png"
 
 export default function Socials() {
     return (
-        <div className="flex bg-black p-4 align-base justify-center gap-4 rounded-b-xl ">
+        <div className="flex bg-black p-4 align-base justify-end gap-4 rounded-b-xl ">
             <a href="https://github.com/jennaly"><img href="https://github.com/jennaly" src={GitHub} /></a>
             <a href="https://twitter.com/jennaly__"><img src={Twitter} /></a>
         </div>
